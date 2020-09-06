9aed3012153fc1f2a1da31e03253310a3c6ce1ec
Mamta <mamtarajput1610@gmail.com>
2020-09-06 22:54:07 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ class App extends Component {
   render(){
   return (
     <div>
+    {/* <div className="header">
       Phone Directory
       </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span><br/>
+        <span> Phone </span>
+      </div>*/}
+    <label for="name">Name: </label>
+    <input id="name" type="text" placeholder="Type Here" defaultValue="Mamta"/>
+    </div>
   );
 }
 }
